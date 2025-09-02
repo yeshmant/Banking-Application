46eb08214bf21465d178aea067a2b9b547988a09
Yeshmant Kumar <yeshmant@gmail.com>
2025-09-03 00:31:50 +0530

    created other Ui pages like fundTransfer, cards, pay-bill

---
+++ src/main/java/com/Banking_Application/controller/investmentsController.java
@@ -0,0 +1,15 @@
+package com.Banking_Application.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class investmentsController {
+
+    @GetMapping("/investments")
+    public String getMethodName() {
+        return "investments";
+    }
+    
+    
+}
